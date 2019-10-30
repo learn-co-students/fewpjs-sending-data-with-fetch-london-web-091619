17c879868437a62faf539b75b9300bc974adc6a3
idmyn <hello@davidmyno.rs>
2019-10-30 16:14:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+function submitData(name, email) {
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: name, email: email})
+  }
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(user => document.querySelector("body").textContent = user.id)
+    .catch(error => document.querySelector("body").textContent = error.message)
+}
